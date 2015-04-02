68fd573d5b67557687723c5724013b84f90065ca
Ramiro Oliva <rosvain@gmail.com>
2015-04-02 15:48:50 -0400

    Getting stops_by_location, stops_by_route and predictions_by_stop data

--- app/bus.php
+++ app/bus.php
@@ -1,12 +1,36 @@
 <?php
-error_reporting(E_ERROR | E_WARNING | E_PARSE);
+//Problem: get all the data about predicting the bus details according to directiona and closest stop
+//What I know:
+//  route:86, this is constant, it will never change
+//  coordinates: longitude and latitude provided by browser
+//  direction: outbound and inbound, depending on what state the buttons are
+//Details needed to get and calculate by nearest stop:
+//  Determine what is the nearest stop depending on users location and preferred direction using stopsbylocation and stopsbyroute api
+//      http://realtime.mbta.com/developer/api/v2/stopsbylocation?api_key=wX9NwuHnZU2ToO7GmGR9uw&lat=42.355375699999996&lon=-71.149031&format=json
+//      This api returns all stops nearby regardles of the route so better filter all other routes out
+//      http://realtime.mbta.com/developer/api/v2/stopsbyroute?api_key=wX9NwuHnZU2ToO7GmGR9uw&route=86&format=json
+//      Get stop
+//  Get details for the nearest stop predictionbystop api:
+//      http://realtime.mbta.com/developer/api/v2/predictionsbystop?api_key=wX9NwuHnZU2ToO7GmGR9uw&stop=1039&format=json
+//      Get predictions by stop of nearest bus
+//      Transform sch_arr_dt (schedule arrival time in epoch time)
+//      Transform pre_away (predicted arrivel time in seconds) 
+//      Get bus current location vehicle_lat and vehicle_lon
+//
+error_reporting(E_ALL);
 require_once 'settings.php';
 define('SYSTEM', 'test');
+define('ROUTE', '86');
+$latitude = '42.355375699999996';
+$longitude = '-71.149031';
+$stop = '1036';
 $settings = get_settings(SYSTEM);
+$stops_by_location = $settings['base_url'] . "stopsbylocation?api_key={$settings['token']}&lat={$latitude}&lon={$longitude}&format=json";
+$stops_by_route = $settings['base_url'] . "stopsbyroute?api_key={$settings['token']}&route={$settings['route']}&format=json";
+$predictions_by_stop = $settings['base_url'] . "predictionsbystop?api_key={$settings['token']}&stop={$stop}&format=json";
 //print_r($settings);
 //echo "<br/>";
 //echo $settings['token'];
-
  function get_data($url)
 {
 $ch = curl_init();
@@ -18,8 +42,9 @@ curl_close($ch);
 return $data;
 }
 
-$data = get_data($settings['base_url']);
-var_dump($data);
+$data = get_data($predictions_by_stop);
+//header('Content-Type: application/javascript');
+//var_dump($data);
 //print_r($data);
 //cleaecho "\n";
 
@@ -27,7 +52,21 @@ var_dump($data);
 //print_r($json);
 //echo "\n";
 $bus_array = json_decode($data,true);
-//var_dump($bus_array);
-echo "\n";
+echo '<pre>';
+print_r($bus_array);
+echo '</pre>';
+//echo "\n";
 //header('Content-Type: application/javascript');
 //echo "raveCallback(". $json.")";
+echo "********\n";
+$data = get_data($stops_by_route);
+$bus_array = json_decode($data,true);
+echo '<pre>';
+print_r($bus_array);
+echo '</pre>';
+echo "********\n";
+$data = get_data($stops_by_location);
+$bus_array = json_decode($data,true);
+echo '<pre>';
+print_r($bus_array);
+echo '</pre>';
--- app/settings.php
+++ app/settings.php
@@ -1,14 +1,16 @@
 <?php
-error_reporting(E_ERROR | E_WARNING | E_PARSE);
+error_reporting(E_ALL);
 function get_settings($system) {
     switch ($system) {
         case 'test':
-            $settings['base_url'] = 'http://realtime.mbta.com/developer/api/v2/predictionsbystop?api_key=wX9NwuHnZU2ToO7GmGR9uw&stop=1036&format=json';
+            $settings['base_url'] = 'http://realtime.mbta.com/developer/api/v2/';
             $settings['token'] = 'wX9NwuHnZU2ToO7GmGR9uw';
+            $settings['route'] = '86';
             break;
         case 'prod':
             $settings['base_url'] = '';
             $settings['token'] = 'wX9NwuHnZU2ToO7GmGR9uw';
+            $settings['route'] = '86';
             break;
     }
     return $settings;
