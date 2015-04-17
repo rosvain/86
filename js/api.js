a3a159d61f0900fde7ca364b8f90b39144454f21
Ramiro Oliva <rosvain@gmail.com>
2015-04-16 20:53:24 -0400

    deleting settings.php from cache, and playing with ajax

--- js/api.js
+++ js/api.js
@@ -23,17 +23,43 @@ $(document).ready(function () {
         });
     });
 });
+var direction = 'outbound';
 
-setInterval(
+//setInterval(
+//        function ()
+//        {
+//            $.ajax({
+//                type: "GET",
+//                url: 'app/prediction.php?latitude=42.355375699999996&longitude=-71.149031&direction='+ direction,
+//                async: true,
+//                dataType: "json",
+//                success: function (data) {
+//                    $('#showdata').html("<p>stop name=" + data.stop_name + " stop id=" + data.stop_id + " minutes=" + data.prediction[0]['pre_away'] + "</p>");
+//                }
+//            });
+//        }, 15000);
+setTimeout(
         function ()
         {
             $.ajax({
                 type: "GET",
-                url: 'app/prediction.php?latitude=42.355375699999996&longitude=-71.149031&direction=outbound',
+                url: 'app/prediction.php?latitude=42.355375699999996&longitude=-71.149031&direction='+ direction,
                 async: true,
                 dataType: "json",
                 success: function (data) {
                     $('#showdata').html("<p>stop name=" + data.stop_name + " stop id=" + data.stop_id + " minutes=" + data.prediction[0]['pre_away'] + "</p>");
                 }
             });
-        }, 60);
+        }, 9000);
+        
+(function pollServerForNewMail() {
+  $.getJSON('/poll_newmail.php', function (response) {
+    if (response.newMail) {
+      alert(
+        "You have received a letter, good sir. " + 
+        "I will have a quick lie-down and be on my way shortly."
+      );
+    }
+    setTimeout(pollServerForMail, 1000);
+  });
+}());
